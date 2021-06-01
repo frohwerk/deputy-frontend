e56bfecf8a064bd3c6e2b4d5ee28b7f81f88fb88
Andreas Frohwerk <frohwerk@users.noreply.github.com>
2021-06-01 16:18:10 +0200

    wip: add support for openid connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  username?: string
+
+  givenName?: string
+
+  ngOnInit(): void {
+    const cookies = parseCookies(document.cookie)
+    const payload = cookies["claims"]
+    console.log(payload)
+    if (payload) {
+      try {
+        const decoded = atob(payload)
+        console.log(decoded)
+        const claims = JSON.parse(decoded)
+        this.givenName = claims?.given_name
+        this.username = claims?.preferred_username
+      } catch(err) {
+        console.log(err)
+      }
+    }
+  }
+
+}
+
+function parseCookies(s: string): Map<string, string> {
+  const map = new Map<string, string>()
+  document.cookie
+    .split(";")
+    .map(v => v?.trim())
+    .map(v => v.split("=", 2))
+    .filter(v => v[0])
+    .forEach(v => map[v[0]] = v[1] ? v[1] : "")
+  return map;
+}
